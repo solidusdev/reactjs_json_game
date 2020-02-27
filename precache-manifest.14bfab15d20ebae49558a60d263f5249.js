7c503230349c5cdfc6eac4c0b86b91e657e06d2c
Ivan <i_cuenca@aol.com>
2020-02-26 19:51:06 -0500

    Updates

---
+++ precache-manifest.14bfab15d20ebae49558a60d263f5249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb76777516cb878d102fca945c7c1ea",
+    "url": "/reactjs_json_game/index.html"
+  },
+  {
+    "revision": "6d434d396b94c811f2b8",
+    "url": "/reactjs_json_game/static/css/main.b69f2731.chunk.css"
+  },
+  {
+    "revision": "49bf931218ef622aaf12",
+    "url": "/reactjs_json_game/static/js/2.24650459.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactjs_json_game/static/js/2.24650459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d434d396b94c811f2b8",
+    "url": "/reactjs_json_game/static/js/main.73d594e7.chunk.js"
+  },
+  {
+    "revision": "f029991ee164e28fb358",
+    "url": "/reactjs_json_game/static/js/runtime-main.601edacd.js"
+  },
+  {
+    "revision": "9b694bc20bced5b4c30860b780d035ca",
+    "url": "/reactjs_json_game/static/media/background.9b694bc2.jpeg"
+  }
+]);
